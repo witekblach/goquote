a318dfe8c24b9834fd0a1c15a0151680f3064797
Witek <witek.blach@gmail.com>
2024-04-13 14:01:09 +0200

    selecting from turso works, now refactor

--- cmd/add.go
+++ cmd/add.go
@@ -1,8 +1,14 @@
 package cmd
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/spf13/cobra"
+	"github.com/tursodatabase/go-libsql"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 // addCmd represents the add command
@@ -19,7 +25,65 @@ This app adds a quote and displays it.`,
 		fmt.Println("add called")
 		fmt.Println("Echo: " + args[0])
 
+		//get connection to db, insert,
+
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file") //probably should use sops
+		}
+
+		dbName := "local.db"
+		primaryUrl := "libsql://" + os.Getenv("TURSO_HOST")
+		authToken := os.Getenv("TURSO_TOKEN")
+
+		dir, err := os.MkdirTemp("", "libsql-*")
+		if err != nil {
+			fmt.Println("Error creating temporary directory:", err)
+			os.Exit(1)
+		}
+		defer os.RemoveAll(dir)
+
+		dbPath := filepath.Join(dir, dbName)
+
+		connector, err := libsql.NewEmbeddedReplicaConnector(dbPath, primaryUrl,
+			libsql.WithAuthToken(authToken),
+		)
+		if err != nil {
+			fmt.Println("Error creating connector:", err)
+			os.Exit(1)
+		}
+		defer connector.Close()
+
+		db := sql.OpenDB(connector)
+		defer db.Close()
+
+		rows, err := db.Query("SELECT * FROM quotes")
+		if err != nil {
+			fmt.Println("AAAAAA PANIC")
+			panic(err)
+		}
+		defer rows.Close()
+		var albums []Quote
+
+		// Loop through rows, using Scan to assign column data to struct fields.
+		for rows.Next() {
+			var alb Quote
+			if err := rows.Scan(&alb.text, &alb.viewed); err != nil {
+				println(fmt.Println(albums, err))
+			}
+			albums = append(albums, alb)
+		}
+		if err = rows.Err(); err != nil {
+			println(fmt.Println(albums, err))
+		}
+
+		println(fmt.Println(albums))
 	},
+}
+
+type Quote struct {
+	text   string
+	viewed int
 }
 
 func init() {
--- go.mod
+++ go.mod
@@ -3,7 +3,15 @@ module github.com/witekblach/goquote
 go 1.21.5
 
 require (
+	github.com/joho/godotenv v1.5.1
+	github.com/spf13/cobra v1.8.0
+	github.com/tursodatabase/go-libsql v0.0.0-20240410174707-5f61d1397cfa
+)
+
+require (
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
+	github.com/libsql/sqlite-antlr4-parser v0.0.0-20240327125255-dbf53b6cbf06 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/exp v0.0.0-20230515195305-f3d0a9c9a5cc // indirect
 )
